3a660d02478a0e5171cb78e41267515d67a4a722
jghaden <jghaden912@gmail.com>
2020-04-20 10:46:40 -0500

    Added function prototypes.

--- FluencyDisorderGenerator/FluencyDisorderGenerator.cpp
+++ FluencyDisorderGenerator/FluencyDisorderGenerator.cpp
@@ -10,21 +10,117 @@
 #define GEN_TYPE_STUTTER		1
 #define GEN_TYPE_INTERJECTION	2
 
+int GetColor(int type);
+std::string StrToLower(const std::string &s);
+bool isPunctuation(char ch);
+std::string ErrorGenerator(const std::string &s);
+
+constexpr unsigned int hash(const char* s, int off = 0)
+{
+	return !s[off] ? 5381 : (hash(s, off + 1) * 33) ^ s[off];
+}
+
+bool isVerbose = false;
+std::string const PUNCTUATION = ".,!?";
+std::vector<std::string> const INTERJECTIONS = { "um", "uh", "oh" };
+std::vector<std::string> const PHRASES = { "yeah but like", "hot diggity dog", "jeepers creepers" };
+
+struct word
+{
+	std::string s;
+	int type = GEN_TYPE_DEFUALT;
+};
+
+int main(int argc, char** argv)
+{
+	srand(time(NULL));
+
+	std::string s, tmp;
+	
+	std::string inputFileName;
+	std::string outputFileName;
+
+	for (int i = 1; i < argc; i++)
+	{
+		s = argv[i];
+		s = StrToLower(s);
+
+		//std::cout << hash(s.c_str()) << ":\t" << s << std::endl;
+		switch (hash(s.c_str()))
+		{
+			case hash("-i"):
+			case hash("-in"):
+				if(argv[i + 1] != '\0' && argv[i + 1][0] != '-')
+					inputFileName = argv[i + 1];
+				break;
+			case hash("-o"):
+			case hash("-out"):
+				if (argv[i + 1] != '\0' && argv[i + 1][0] != '-')
+					outputFileName = argv[i + 1];
+				break;
+			case hash("-h"):
+			case hash("-help"):
+				println("\t-h,-help\tDisplays this help screen");
+				println("\t-i,-in\t\tInput file for parsing");
+				println("\t-o,-out\t\tOutput file after parsing");
+				exit(0);
+				break;
+			case hash("-v"):
+			case hash("-verbose"):
+				isVerbose = true;
+				break;
+		}
+	}
+
+	if (argv[1] != '\0')
+	{
+		if (!inputFileName.empty() && !outputFileName.empty())
+		{
+			std::ifstream inFile;
+			std::ofstream outFile;
+
+			inFile.open(inputFileName);
+			outFile.open(outputFileName);
+			if (inFile.is_open())
+			{
+				while (!inFile.eof())
+				{
+					getline(inFile, tmp);
+					outFile << '[' << ErrorGenerator(StrToLower(tmp)) << "], [" << StrToLower(tmp) << ']' << std::endl;
+				}
+			} else
+			{
+				println("ERROR::MISSING::INPUT_FILE: " + inputFileName);
+			}
+
+			inFile.close();
+		} else
+		{
+			if (inputFileName.empty())
+				println("ERROR::MISSING::INPUT_FILE_NAME");
+			if (outputFileName.empty())
+				println("ERROR::MISSING::OUTPUT_FILE_NAME");
+		}
+	}
+
+	return 0;
+}
+
 int GetColor(int type)
 {
 	int c;
 
 	switch (type)
 	{
-		case GEN_TYPE_STUTTER:
-			c = HIGHLIGHT_STUTTER;
-			break;
-		case GEN_TYPE_INTERJECTION:
-			c = HIGHLIGHT_INTERJECTION;
-			break;
-		case GEN_TYPE_DEFUALT:
-			c = HIGHLIGHT_DEFAULT;
-			break;
+	case GEN_TYPE_STUTTER:
+		c = HIGHLIGHT_STUTTER;
+		break;
+	case GEN_TYPE_INTERJECTION:
+		c = HIGHLIGHT_INTERJECTION;
+		break;
+	case GEN_TYPE_DEFUALT:
+		c = HIGHLIGHT_DEFAULT;
+		break;
 	}
 
 	return c;
@@ -48,27 +144,10 @@ std::string StrToLower(const std::string &s)
 	return out;
 }
 
-constexpr unsigned int hash(const char* s, int off = 0)
-{
-	return !s[off] ? 5381 : (hash(s, off + 1) * 33) ^ s[off];
-}
-
-bool isVerbose = false;
-
-std::string const PUNCTUATION = ".,!?";
-std::vector<std::string> const INTERJECTIONS = { "um", "uh", "oh" };
-std::vector<std::string> const PHRASES = { "yeah but like", "hot diggity dog", "jeepers creepers" };
-
 bool isPunctuation(char ch)
 {
 	return (PUNCTUATION.find(ch) != std::string::npos);
 }
-
-struct word
-{
-	std::string s;
-	int type = GEN_TYPE_DEFUALT;
-};
 
 std::string ErrorGenerator(const std::string &s)
 {
@@ -156,79 +235,4 @@ std::string ErrorGenerator(const std::string &s)
 	}
 
 	return out;
-}
-
-int main(int argc, char** argv)
-{
-	srand(time(NULL));
-
-	std::string s, tmp;
-	
-	std::string inputFileName;
-	std::string outputFileName;
-
-	for (int i = 1; i < argc; i++)
-	{
-		s = argv[i];
-		s = StrToLower(s);
-
-		//std::cout << hash(s.c_str()) << ":\t" << s << std::endl;
-		switch (hash(s.c_str()))
-		{
-			case hash("-i"):
-			case hash("-in"):
-				if(argv[i + 1] != '\0' && argv[i + 1][0] != '-')
-					inputFileName = argv[i + 1];
-				break;
-			case hash("-o"):
-			case hash("-out"):
-				if (argv[i + 1] != '\0' && argv[i + 1][0] != '-')
-					outputFileName = argv[i + 1];
-				break;
-			case hash("-h"):
-			case hash("-help"):
-				println("\t-h,-help\tDisplays this help screen");
-				println("\t-i,-in\t\tInput file for parsing");
-				println("\t-o,-out\t\tOutput file after parsing");
-				exit(0);
-				break;
-			case hash("-v"):
-			case hash("-verbose"):
-				isVerbose = true;
-				break;
-		}
-	}
-
-	if (argv[1] != '\0')
-	{
-		if (!inputFileName.empty() && !outputFileName.empty())
-		{
-			std::ifstream inFile;
-			std::ofstream outFile;
-
-			inFile.open(inputFileName);
-			outFile.open(outputFileName);
-			if (inFile.is_open())
-			{
-				while (!inFile.eof())
-				{
-					getline(inFile, tmp);
-					outFile << '[' << ErrorGenerator(StrToLower(tmp)) << "], [" << StrToLower(tmp) << ']' << std::endl;
-				}
-			} else
-			{
-				println("ERROR::MISSING::INPUT_FILE: " + inputFileName);
-			}
-
-			inFile.close();
-		} else
-		{
-			if (inputFileName.empty())
-				println("ERROR::MISSING::INPUT_FILE_NAME");
-			if (outputFileName.empty())
-				println("ERROR::MISSING::OUTPUT_FILE_NAME");
-		}
-	}
-
-	return 0;
 }
